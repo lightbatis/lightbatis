b95b6e1e039fd3bc2defd035d92aeed47bd673ac
李飞 <lifei@aeotrade.com>
2020-07-21 09:55:14 +0800

    添加对 PageHelper 的兼容性支持

---
+++ lightbatis-pagelisthelper/src/main/java/titan/lightbatis/pagehelper/PageListHelper.java
@@ -0,0 +1,23 @@
+package titan.lightbatis.pagehelper;
+
+import com.github.pagehelper.PageHelper;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.session.RowBounds;
+import titan.lightbatis.result.PageList;
+
+import java.util.List;
+
+public class PageListHelper extends PageHelper {
+    @Override
+    public boolean skip(MappedStatement ms, Object parameterObject, RowBounds rowBounds) {
+        return super.skip(ms, parameterObject, rowBounds);
+    }
+
+    @Override
+    public Object afterPage(List pageList, Object parameterObject, RowBounds rowBounds) {
+        if (pageList instanceof PageList){
+            return pageList;
+        }
+        return super.afterPage(pageList, parameterObject, rowBounds);
+    }
+}
