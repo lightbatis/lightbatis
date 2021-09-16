b261a0ea1242af2a8b90d0141e97a915a5e7b042
lifei114 <lifei114@126.com>
2021-09-16 16:05:19 +0800

    清空多余的类

--- lightbatis-common/src/main/java/titan/lightbatis/common/ResultCode.java
+++ lightbatis-common/src/main/java/titan/lightbatis/common/ResultCode.java
@@ -9,7 +9,10 @@ public enum ResultCode implements IErrorCode {
     FAILED(500, "操作失败"),
     VALIDATE_FAILED(404, "参数检验失败"),
     UNAUTHORIZED(401, "暂未登录或token已经过期"),
+    UNREGISTERED(402,"用户未注册"),
+    DETECT_FAILED(410,"检测失败"),
     FORBIDDEN(403, "没有相关权限");
+
     private long code;
     private String message;
 
